b87e7c389da0d6f97244ca9a898926ab8638610d
evefaith29 <152077901+evefaith29@users.noreply.github.com>
2023-12-04 09:01:58 +0800

    memperbarui gambar

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -10,19 +10,19 @@ const Project = () => {
             <p>Throughout my educational journey</p>
                 <div className="row gtr-150">
                     <div className="col-4 col-12-medium">
-                    <span className="image fit"><img src="/public/images/OSIS.jpg" alt /></span>
+                    <span className="image fit"><img src="https://github.com/evefaith29/source/blob/master/public/images/OSIS.jpg?raw=true" alt /></span>
                     <h3>OSIS SMA</h3>
                     <p>Through organizing events and collaborating on performances, this role allows me to express my passion for music while fostering unity among students, making it a thrilling experience that enhances my organizational and leadership skills, and brings joy to the school through the universal language of music.</p>
                     </div>
                     <div className="col-4 col-12-medium">
-                    <span className="image fit"><img src="/public/images/VOCS.svg" alt /></span>
+                    <span className="image fit"><img src="https://raw.githubusercontent.com/evefaith29/source/1a0945fc4b5043926a7ba6f17cdfee2774bca0d7/public/images/VOCS.svg" alt /></span>
                     <h3>VOCS</h3>
                     <p>Through my experience with VOCS, I have not only developed vocal skills and musicality but also expanded my social network by meeting and collaborating with talented students from various computer science programs.
                     The journey with VOCS has not only enriched my musical abilities but also fostered a sense of camaraderie, creating unforgettable memories that will forever be cherished in my time as a computer science student.
                     </p>
                     </div>
                     <div className="col-4 col-12-medium">
-                    <span className="image fit"><img src="/public/images/GS.png" alt /></span>
+                    <span className="image fit"><img src="https://github.com/evefaith29/source/blob/master/public/images/GS.png?raw=true" alt /></span>
                     <h3>Gema Sangkakala Choir</h3>
                     <p>As a singer in the Gema Sangkakala Choir from North Sulawesi, I've had a rich and meaningful experience. Rehearsals and performances have enhanced my vocal skills, fostering camaraderie with talented choir members. From local shows to national competitions, this choir has been a platform to appreciate North Sulawesi's cultural richness through music. The organization has also played a crucial role in my personal and collaborative development, creating lasting memories.</p>
                     </div>
